5ee114049d69617c3cb6120f12b603074e37a9d2
Nico Neiman <niconeiman@gmail.com>
2023-11-25 17:35:18 -0600

    schema definition

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const app = require("./server");
-
-const PORT = 3001;
-app.listen(PORT, () => {
-	console.log(`Server listening at port ${PORT}`);
-});
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import app from "./server";
+
+const PORT = 3001;
+app.listen(PORT, () => {
+	console.log(`Server listening at port ${PORT}`);
+});
