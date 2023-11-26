967f01762810d4195263f05885d7a2bb68ef6800
Nico Neiman <niconeiman@gmail.com>
2023-11-26 17:08:02 -0600

    update handlers

--- src/api/handlers/product.ts
+++ src/api/handlers/product.ts
@@ -12,14 +12,29 @@ export class ProductRoutes {
 
 			res.status(200);
 			res.json({ data: { products: user.products } });
-		} catch (e) {}
+		} catch (error) {
+			res.status(500);
+			res.json({ error });
+		}
 	}
 
-	async getProductById(req, res) {}
+	async getProductById(req, res) {
+		try {
+			const product = await prisma.product.findUnique({
+				where: { id: req.params.id, belongsTo: req.user.id },
+			});
+
+			res.status(200);
+			res.json({ data: { product } });
+		} catch (error) {
+			res.status(500);
+			res.json({ error });
+		}
+	}
 
 	async createNewProduct(req, res) {
 		try {
-			await prisma.product.create({
+			const newProduct = await prisma.product.create({
 				data: {
 					name: req.body.name,
 					created_at: new Date(),
@@ -29,15 +44,44 @@ export class ProductRoutes {
 			});
 
 			res.status(200);
-			res.json({ message: "Product succesfully created" });
-		} catch (e) {
-			console.log(e);
+			res.json({
+				data: { newProduct, message: "Product succesfully created" },
+			});
+		} catch (error) {
 			res.status(500);
-			res.json({ error: e });
+			res.json({ error });
 		}
 	}
 
-	async updateProduct(req, res) {}
+	async updateProduct(req, res) {
+		try {
+			const product = await prisma.product.update({
+				where: { id: req.params.id, belongsTo: req.user.id },
+				data: {
+					name: req.body.name,
+					updated_at: new Date(),
+				},
+			});
 
-	async deleteProduct(req, res) {}
+			res.status(200);
+			res.json({ data: { product, message: "Product succesfully updated" } });
+		} catch (error) {
+			res.status(500);
+			res.json({ error });
+		}
+	}
+
+	async deleteProduct(req, res) {
+		try {
+			await prisma.product.delete({
+				where: { id: req.params.id },
+			});
+
+			res.status(200);
+			res.json({ message: "Product succesfully deleted" });
+		} catch (error) {
+			res.status(500);
+			res.json({ error });
+		}
+	}
 }
