432e30b5a05826a42c1bc7c567122fe9efe4e6b6
Nico Neiman <niconeiman@gmail.com>
2023-11-25 19:44:27 -0600

    router

---
+++ src/router.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/product", (_, res) => {
+	res.status(200);
+	res.json({ message: "hi señor" });
+});
+router.get("/product/:id", () => {});
+router.put("/product/:id", () => {});
+router.post("/product", () => {});
+router.delete("/product/:id", () => {});
+
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.put("/update/:id", () => {});
+router.post("/update/", () => {});
+router.delete("/update/:id", () => {});
+
+router.get("/update-point", () => {});
+router.get("/update-point/:id", () => {});
+router.put("/update-point/:id", () => {});
+router.post("/update-point/", () => {});
+router.delete("/update-point/:id", () => {});
+
+export default router;
