717ba5070901f02223e4c1a2818963eeb4d94ca8
Nico Neiman <niconeiman@gmail.com>
2023-12-02 08:31:35 -0600

    user handlers into a class & test setup

---
+++ src/api/handlers/user.ts
@@ -0,0 +1,70 @@
+import { ErrorType } from "../entities/errors";
+import { createJWT, hashPassword, isValidPassword, prisma } from "../utils";
+
+const genericErrorMessage = "Invalid credentials";
+
+export class UserHandlers {
+	constructor() {}
+
+	async signIn(req, res, next) {
+		try {
+			const { username, password } = req.body;
+
+			const user = await prisma.user.findUnique({
+				where: { username },
+			});
+
+			if (!user) {
+				throw new Error(genericErrorMessage);
+			}
+
+			const isValid = await isValidPassword(password, user.password);
+
+			if (!isValid) {
+				throw new Error(genericErrorMessage);
+			}
+
+			const token = createJWT(user);
+			res.status(200);
+			res.json({ token, user });
+		} catch (e) {
+			// TODO check the error message from Prisma and add specificity to the error
+			e.type = ErrorType.INPUT;
+			next(e);
+		}
+	}
+
+	async signUp(req, res, next) {
+		try {
+			const { username, password } = req.body;
+
+			const isExistingUsername = await prisma.user.findUnique({
+				where: { username },
+			});
+
+			if (isExistingUsername) {
+				throw new Error(
+					"Username not available, please choose a different one."
+				);
+			}
+
+			const user = await prisma.user.create({
+				data: {
+					username: username,
+					password: await hashPassword(password),
+					created_at: new Date(),
+					updated_at: new Date(),
+				},
+			});
+
+			const token = createJWT(user);
+
+			res.status(200);
+			res.json({ token });
+		} catch (e) {
+			e.type = ErrorType.INPUT;
+
+			next(e);
+		}
+	}
+}
--- src/api/routes/public-routes.ts
+++ src/api/routes/public-routes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
-import { signUp, signIn } from "../handlers/user";
 import { credentialsValidationChain } from "../middleware";
+import { UserHandlers } from "../handlers/user";
 
 const router = Router();
-
+const user = new UserHandlers();
 router.get("/", (req, res) => {
 	res.status(200);
 	res.json({ message: "hello" });
 });
 
-router.post("/sign-up", credentialsValidationChain, signUp);
-router.post("/sign-in", credentialsValidationChain, signIn);
+router.post("/sign-up", credentialsValidationChain, user.signUp);
+router.post("/sign-in", credentialsValidationChain, user.signIn);
 
 export { router as publicRoutes };
